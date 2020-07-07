c5ac39e9639573550c49373f3f24e87aa61a2572
Ahmed Abdulrahman <ahmed751995@riseup.net>
2020-07-08 00:23:52 +0200

    pagination and CORS

--- API Development and Documentation/class-demos/flaskr/__init__.py
+++ API Development and Documentation/class-demos/flaskr/__init__.py
@@ -1,14 +1,41 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request, abort
+from flask_cors import CORS, cross_origin
+from .models import setup_db, Book
 
 def create_app(test_config=None):
     app = Flask(__name__)
+    setup_db(app)
+    CORS(app)
 
-    @app.route('/')
-    def hello():
-        return jsonify({'content': 'good morning'})
-
-    @app.route('/smiley')
-    def smiley():
-        return ":)"
+    @app.after_request
+    def after_request(response):
+        response.headers.add('Access-Control-Allowed-Headers', 'Content-Type, Authorization')
+        response.headers.add('Access-Control-Allowed-Methods', 'GET, POST, PATCH, DELETE, OPTIONS')
+        return response
     
+    @app.route('/books')
+    def hello():
+        page = request.args.get('page', 1, type=int)
+        start = (page - 1) * 2
+        end = start + 2
+        books = Book.query.all()
+        formatted_books = [book.format() for book in books]
+        
+        return jsonify({
+            'success': True,
+            'books': formatted_books[start:end],
+            'total-books': len(formatted_books)})
+
+    @app.route('/books/<int:book_id>')
+    def get_specific_book(book_id):
+        book = Book.query.filter(Book.id == book_id).one_or_none()
+        if book == None:
+            abort(404)
+        else:
+            return jsonify({
+                'success': True,
+                'book': book.format()
+            })
+    
+        
     return app
